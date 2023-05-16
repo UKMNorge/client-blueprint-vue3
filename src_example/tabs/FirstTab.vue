ae3e8421692710e218bcfa2616f70ab63854ad03
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-05-16 18:45:26 +0200

    Import fra ukm-components-vue3 på src_example

--- src_example/tabs/FirstTab.vue
+++ src_example/tabs/FirstTab.vue
@@ -2,12 +2,19 @@
     <div>
         <h1>First Tab</h1>
         <ChildComponent ref="childArrangementer" />
+        
+        <h2>Fra UKM Komponenter</h2>
+        <UKMTable msg='Hello Table test' color="red" />
+
     </div>
 </template>
   
 <script setup lang="ts">
     import ChildComponent from '../components/ChildComponent.vue';
     import { ref, onMounted } from 'vue';
+    // Import fra UKM Komponenter
+    import { UKMTable } from 'ukm-components-vue3';
+
     const childArrangementer = ref();
     
     onMounted(() => {
