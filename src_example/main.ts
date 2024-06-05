895f48461df12cb1a9158910183a9e251825dbc5
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-06-05 23:51:25 +0200

    Oppdaterer main.ts for å støtte Vuetify og ukm-components-vue3
    Det er også integrert spaInteraction tilgjengelig på window. Alle Vue filer har tilgang til samme instanse som gjør det sentralisert for å kjøre kall og vise feilmeldinger

--- src_example/main.ts
+++ src_example/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp, h, ref } from "vue";
 import App from "./App.vue";
 
 import hljs from "highlight.js/lib/core";
@@ -8,11 +8,49 @@ import xml from "highlight.js/lib/languages/xml";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 import "highlight.js/styles/base16/dracula.css";
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import "../node_modules/ukm-components-vue3/dist/style.css";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
 hljs.registerLanguage("xml", xml);
 hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("css", css);
 
-import "../node_modules/ukm-components-vue3/dist/style.css";
+// Register the Interaction object globally so it can be accessed from other vue components or scripts
+// InteractionObject is used to show notifications, confirm dialogs, and other interactions
+import { InteractionObject } from 'ukm-components-vue3';
+import { SPAInteraction } from 'ukm-components-vue3';
+
+// Programmatically mount the InteractionObject
+const interactionObjectInstance = createApp(InteractionObject);
+
+// Ensure Vuetify is used by the interactionObjectInstance
+interactionObjectInstance.use(vuetify);
+
+const interactionInstance = interactionObjectInstance.mount(document.createElement('div'));
+
+// Append the interactionObjectInstance to the body
+document.body.appendChild(interactionInstance.$el);
+
+console.log('interactionInstance');
+console.log(interactionInstance);
+
+var ajaxurl : string = (<any>window).ajaxurl; // Kommer fra global
+
+if(ajaxurl == undefined || ajaxurl == null) {
+    throw new Error('ajaxurl is not defined');
+}
+
+(<any>window).spaInteraction = new SPAInteraction(interactionInstance, ajaxurl);
 
 const app = createApp(App);
 
@@ -22,7 +60,7 @@ import { Director } from 'ukm-spa/Director';
 var director = new Director();
 (<any>window).director = director;
 
-
 app.use(hljsVuePlugin);
+app.use(vuetify);
 
 app.mount("#app");
